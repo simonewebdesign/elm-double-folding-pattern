f9238d4b9a587b694a743fcc4264ce79138929ef
Simone Vittori <hello@simonewebdesign.it>
2016-03-11 16:51:43 +0000

    Events as a parallel stream

--- Main.elm
+++ Main.elm
@@ -1,6 +1,6 @@
 import Html exposing (Html, div, button, text, input, form)
 import Html.Attributes exposing (type')
-import Html.Events exposing (onClick)
+import Html.Events exposing (on, onClick, targetValue)
 import Signal exposing (Address)
 import Keyboard
 import Mouse
@@ -9,12 +9,25 @@ import Time
 
 type alias Model =
   { counter: Int
+  , cardNumber: String
   }
 
 
 initialModel : Model
 initialModel =
   { counter = 0
+  , cardNumber = ""
+  }
+
+
+type alias ViewState =
+  { digits: String
+  }
+
+
+initialViewState : ViewState
+initialViewState =
+  { digits = ""
   }
 
 
@@ -23,9 +36,19 @@ model =
   Signal.foldp update initialModel inputs
 
 
+viewState : Signal ViewState
+viewState =
+  Signal.foldp render initialViewState events.signal
+
+
 actions : Signal.Mailbox Action
 actions =
   Signal.mailbox NoOp
+
+
+events : Signal.Mailbox Event
+events =
+  Signal.mailbox Never
 
 
 inputs : Signal Action
@@ -47,19 +70,21 @@ inputs =
 
 main : Signal Html
 main =
-  Signal.map (view actions.address) model
+  Signal.map2 (view actions.address) viewState model
 
 
 -- VIEWS
 
 
-view : Address Action -> Model -> Html
-view address model =
+view : Address Action -> ViewState -> Model -> Html
+view address state model =
   div []
     [ button [ onClick address Decrement ] [ text "-" ]
     , div [] [ text (toString model.counter) ]
     , button [ onClick address Increment ] [ text "+" ]
     , creditCardForm
+    , "state: " ++ (toString state) |> text
+    , "model: " ++ (toString model) |> text
     ]
 
 
@@ -76,6 +101,7 @@ creditCardInput : Html
 creditCardInput =
   input
     [ type' "text"
+    , on "input" targetValue (\digit -> Signal.message events.address (DigitEntry digit))
     ] []
 
 
@@ -90,6 +116,7 @@ type Action
   = NoOp
   | Increment
   | Decrement
+  | CompletedCardNumber String
 
 
 update : Action -> Model -> Model
@@ -98,3 +125,17 @@ update action model =
     NoOp -> model
     Increment -> { model | counter = model.counter + 1 }
     Decrement -> { model | counter = model.counter - 1 }
+    CompletedCardNumber newNumber ->
+      { model | cardNumber = newNumber }
+
+
+type Event
+ = Never
+ | DigitEntry String
+
+
+render : Event -> ViewState -> ViewState
+render event state =
+  case event of
+    Never -> state
+    DigitEntry newDigit -> { state | digits = state.digits ++ newDigit }
