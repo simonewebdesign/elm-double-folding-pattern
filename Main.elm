27573ec3f168bc263a24a208661651a959076257
Simone Vittori <hello@simonewebdesign.it>
2016-03-11 12:11:21 +0000

    Add type annotations

--- Main.elm
+++ Main.elm
@@ -1,15 +1,20 @@
-import Html exposing (div, button, text)
+import Html exposing (Html, div, button, text)
 import Html.Events exposing (onClick)
 import StartApp.Simple as StartApp
 
+type alias Model = Int
 
+
+initialModel : Model
+initialModel = 0
+
+
+main : Signal Html
 main =
-  StartApp.start { model = model, view = view, update = update }
+  StartApp.start { model = initialModel, view = view, update = update }
 
 
-model = 0
-
-
+view : Signal.Address Action -> Model -> Html
 view address model =
   div []
     [ button [ onClick address Decrement ] [ text "-" ]
@@ -21,6 +26,7 @@ view address model =
 type Action = Increment | Decrement
 
 
+update : Action -> Model -> Model
 update action model =
   case action of
     Increment -> model + 1
